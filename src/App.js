88163fce0a2e8e53e29eafa62ff82ccfa0eca9e2
Flor <flordemari.calla@mail.udp.cl>
2021-08-15 17:56:06 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, {} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { HashRouter as Router, Route } from "react-router-dom";
 
 //COMPONENTES
 import Modo from './componentes/Modo';
